ab5e03410d835affd403d25d6a4d5d34a61c1a8b
Harrison White <hbwhite1996@gmail.com>
2019-05-21 15:20:55 -0500

    updated license

--- NZXT-RGB-Mac-GUI/AppDelegate.h
+++ NZXT-RGB-Mac-GUI/AppDelegate.h
@@ -5,6 +5,19 @@
 //  Created by Harrison White on 5/17/19.
 //  Copyright © 2019 Harrison White. All rights reserved.
 //
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #import <Cocoa/Cocoa.h>
 
